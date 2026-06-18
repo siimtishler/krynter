6f2e1ba6905c317c551343929131a0659f2cacfc
Siim Tišler <siim.tishlr@gmail.com>
2026-06-18 20:55:11 +0300

    Simplified BE api module

--- backend/api/api.py
+++ backend/api/api.py
@@ -1,7 +1,5 @@
-import geopandas as gpd
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException
 
-from backend.core.config import PROJECT_ROOT, config
 from backend.core.logging import logger
 from backend.geo.geo import (
     DEFAULT_POI_LIMIT,
@@ -18,33 +16,16 @@ def read_root():
     return {"Hello": "World"}
 
 
-@router.get("/debug/kerese-gpkg.geojson")
-def get_kerese_gpkg_geojson():
-    kerese_path = PROJECT_ROOT / "experiments" / "kerese_tnv.gpkg"
-    if not kerese_path.exists():
-        raise HTTPException(status_code=404, detail="Kerese GPKG not found")
-
-    gdf = gpd.read_file(kerese_path)
-    if gdf.crs is None:
-        gdf = gdf.set_crs(config.data_crs)
-
-    gdf = gdf.to_crs(config.frontend_crs)
-    return Response(content=gdf.to_json(), media_type="application/geo+json")
-
-
 @router.get("/search_address")
 def return_parcel_from_address(address: str, top_n: int = DEFAULT_POI_LIMIT):
     parcel: Parcel = get_parcel_cadastre_series_from_address(address)
     logger.debug(parcel)
     if parcel is None:
         return {"error": "parcel get failed via address search"}
-    coords = parcel.get_parcel_geometry_geojson()
-    centre_point = parcel.get_center_point_coords_geojson()
     return {
         "Aadress": Parcel.to_dict(parcel.parcel),
-        "coordinates": coords,
-        "centre_point": centre_point,
         "nearby_pois": parcel.get_nearby_pois(top_n=top_n),
+        "noise_levels": parcel.get_surrounding_noise_level(20),
     }
 
 
@@ -54,13 +35,10 @@ def return_address_from_cadastre(cadastre_code: str, top_n: int = DEFAULT_POI_LI
     logger.debug(parcel)
     if parcel is None:
         return {"error": "parcel get failed via cadastre search"}
-    coords = parcel.get_parcel_geometry_geojson()
-    centre_point = parcel.get_center_point_coords_geojson()
     return {
         "Aadress": Parcel.to_dict(parcel.parcel),
-        "coordinates": coords,
-        "centre_point": centre_point,
         "nearby_pois": parcel.get_nearby_pois(top_n=top_n),
+        "noise_levels": parcel.get_surrounding_noise_level(20),
     }
 
 
